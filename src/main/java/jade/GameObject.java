75529ae07df6f8e73afa3ede1a81e4001674a896
Olivier Stierer <106349623+nanookOlive@users.noreply.github.com>
2024-04-03 12:15:08 +0200

    finish GameObject and component class

--- src/main/java/jade/GameObject.java
+++ src/main/java/jade/GameObject.java
@@ -10,4 +10,48 @@ public class GameObject {
         this.name=name;
 
     }
+
+    public <T extends Component> T getComponent(Class<T> componentClass){
+        for( Component c : components){
+            if(componentClass.isAssignableFrom(c.getClass())){
+                try{
+                    return componentClass.cast(c);
+                } catch(ClassCastException e){
+                    e.printStackTrace();
+                    assert false :"Erreur de conversion de component.";
+                }
+
+            }
+        }
+        return null;
+    }
+
+    public <T extends Component> void removeComponent(Class<T> componentClass){
+
+        for(int a = 0;a < components.size();a ++){
+            Component c = components.get(a);
+            if(componentClass.isAssignableFrom(c.getClass())){
+                components.remove(a);
+                return;
+            }
+
+        }
+    }
+
+    public void addComponent(Component component){
+        this.components.add(component);
+        component.gameObject= this;
+    }
+
+    public void update(float dt){
+        for(int a=0; a < components.size() ; a++){
+            components.get(a).update(dt);
+        }
+    }
+
+    public void start(){
+        for(int a =0 ; a < components.size();a++){
+            components.get(a).start();
+        }
+    }
 }

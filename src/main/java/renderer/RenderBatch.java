98133682c5028bb095072c4430e4d7f049d1ee3b
Olivier Stierer <106349623+nanookOlive@users.noreply.github.com>
2024-04-03 16:05:39 +0200

    working on RenderBatch

--- src/main/java/renderer/RenderBatch.java
+++ src/main/java/renderer/RenderBatch.java
@@ -1,4 +1,50 @@
 package renderer;
 
+import components.SpriteRenderer;
+
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
 public class RenderBatch {
+
+    private final int POS_SIZE =2;
+    private final int COLOR_SIZE=4;
+
+    private final int POS_OFFSET = 0;
+    private final int COLOR_OFFSET = POS_OFFSET + POS_SIZE * Float.BYTES;
+    private final int VERTEX_SIZE = 6;
+    private final int VERTEX_SIZE_BYTES = VERTEX_SIZE * Float.BYTES;
+    private SpriteRenderer[] sprites;
+    private int numSprites;
+    private boolean hasRoom;
+    private float[] vertices;
+    private int vaoID, vboID;
+    private shader shader;
+    private int maxBatchSize;
+
+    public RenderBatch(int maxBatchSize){
+
+        shader = new shader("assets/shaders/default.glsl");
+        shader.compile();
+        this.sprites = new SpriteRenderer[maxBatchSize];
+        this.maxBatchSize=maxBatchSize;
+
+        vertices = new float[maxBatchSize * 4 * VERTEX_SIZE];
+
+        this.numSprites=0;
+        this.hasRoom=true;
+
+    }
+
+    public void start(){
+
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER,vboID);
+        glBufferData(GL_ARRAY_BUFFER,vertices.length * Float.BYTES, GL_DYNAMIC_DRAW );
+    }
+
 }

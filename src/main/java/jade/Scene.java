aec6b86e1f89ae3d2db822ab9109bed371bbadac
Olivier Stierer <106349623+nanookOlive@users.noreply.github.com>
2024-04-03 12:49:45 +0200

    add start and addGameObjectToScene to Scen class

--- src/main/java/jade/Scene.java
+++ src/main/java/jade/Scene.java
@@ -1,14 +1,40 @@
 package jade;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract  class Scene {
 
 
     protected  Camera camera;
-    public Scene(){}
+    private boolean isRunning =false;
+    private List<GameObject> gameObjects = new ArrayList<>() ;
+    public Scene(){
+
+
+    }
 
 
     public void init(){}
+
+    public void start(){
+
+        for(GameObject go : gameObjects){
+            go.start();
+        }
+    }
+    public void addGameObjectToScene(GameObject go){
+
+        if(!isRunning){
+            gameObjects.add(go);
+        } else{
+
+            gameObjects.add(go);
+            go.start();
+        }
+    }
     public abstract void update(float dt);
+
 
 
 }

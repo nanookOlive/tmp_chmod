70f889ad10563df0baa2d8a11a584df82196d5ff
Olivier Stierer <106349623+nanookOlive@users.noreply.github.com>
2024-04-17 10:34:48 +0200

    GAMEOBJECT-add zIndex and getter

--- src/main/java/jade/GameObject.java
+++ src/main/java/jade/GameObject.java
@@ -8,15 +8,18 @@ public class GameObject {
     private String name;
     private List<Component> components;
     public Transform transform;
+    private int zIndex;
     public GameObject(String name){
         this.name=name;
         this.components = new ArrayList<>();
         this.transform = new Transform();
+        this.zIndex=0;
 
     }
 
-    public GameObject(String name, Transform transform){
+    public GameObject(String name, Transform transform, int zIndex){
         this.name=name;
+        this.zIndex=zIndex;
         this.components=new ArrayList<>();
         this.transform=transform;
     }
@@ -65,7 +68,7 @@ public class GameObject {
         }
     }
 
-//    public String getName(){
-//        return this.name;
-//    }
+    public int zIndex(){
+        return this.zIndex;
+    }
 }
